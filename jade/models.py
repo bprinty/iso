6cd86055babab4bb820c4545862016377db2ab0a
bprinty <bprinty@gmail.com>
2017-09-20 10:02:31 -0500

    added scaffolds for threshold models

--- jade/models.py
+++ jade/models.py
@@ -19,74 +19,18 @@ from sklearn.base import BaseEstimator, TransformerMixin
 # ------------
 class Threshold(BaseEstimator):
     """
-    Simple threshold model for single column. If no threshold is specified,
-    this model will use the bounds and step parameters to brute-force
-    optimize a thresold that maximizes f1 score for predicting variants.
-
-    Args:
-        idx (str): Column to parameterize event-aware estimator for.
-        thresh (int): Feature value to use as threshold in calling event.
-        bounds (list): Bounds for parameterizing threshold.
-        step (float): Step size to use in parameterizing new threshold.
+    TODO: this. threshold using single feature column -- this requires
+          that a FeatureTransform has been applied as the last step of
+          the transform.
     """
-    
-    def __init__(self, idx, thresh=0.5, bounds=[0, 1], step=0.001):
-        self.idx = idx
-        self.thresh = thresh
-        self._thresh = thresh
-        self.bounds = bounds
-        self.step = step
-        return
-
-    def reset(self):
-        self.thresh = self._thresh
-        return
-
-    def fit(self, X, y):
-        arr = numpy.array([X[idx][self.idx] for idx in range(0, len(X))])
-        opt = optimize.brute(
-            lambda x: - metrics.f1_score(y, arr >= x),
-            ranges=(slice(self.bounds[0], self.bounds[1], self.step),)
-        )
-        self.thresh = opt[0]
-        return self
-
-    def predict(self, X):
-        arr = numpy.array([X[idx][self.idx] for idx in range(0, len(X))])
-        return arr >= self.thresh
+    pass
 
 
 class MultiThreshold(BaseEstimator):
     """
     TODO: this. Like above but with multiple columns.
     """
-    
-    def __init__(self, **kwargs):
-        self.thresh = kwargs
-        self.idx = dict(zip(kwargs.keys(), [None] * len(kwargs)))
-        return
-
-    def fit(self, X, y):
-        # TODO: enable column-specific threshold parameterization,
-        #       that utilizes the thresholds from all columns to optimize
-        #       all 'None' threshold values. Maybe use basinhopping?
-        # for key in self.thresh:
-        #     if self.thresh[key] is None:
-        #         arr = numpy.array([X[idx][self.idx] for idx in range(0, len(X))])
-        #         opt = optimize.brute(
-        #             lambda x: - metrics.f1_score(y, arr >= x),
-        #             ranges=(slice(self.bounds[0], self.bounds[1], self.step),)
-        #         )
-        #         self.thresh = opt[0]
-        return self
-
-    def predict(self, X):
-        res = numpy.array([True] * len(X), dtype=bool)
-        for col in self.thresh.keys():
-            cidx = self.idx[col]
-            arr = numpy.array([X[idx][cidx] for idx in range(0, len(X))]) >= self.thresh[col]
-            res &= arr
-        return res
+    pass
 
 
 class IdentityModel(BaseEstimator):
